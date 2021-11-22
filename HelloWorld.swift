800a1eae89b10f8c92674e27a234d522a58f9196
Ubuntu <ubuntu@ip-172-31-81-87.ec2.internal>
2021-11-22 18:26:15 +0000

    helloworld

---
+++ HelloWorld.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Ahmad El-khawaldeh
+//  Created on 2021-20-11
+//  Version 1.0
+//  Copyright (c) 2020 MTHS. All rights reserved                                                               
+//                                                                                  
+//  The Hello World! program
+//
+
+print("Hello, World")
